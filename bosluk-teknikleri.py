634d5a7563a83bb81f0097e2a28443aae18e7cec
Cagla Nur Sorkun <caglasorkun@gmail.com>
2023-02-05 21:03:01 +0300

    \n ve \t kullanım kodu commitlendi

---
+++ bosluk-teknikleri.py
@@ -0,0 +1,13 @@
+# boşluk kullanma teknikleri \n, \t
+metin=""" Some love too little, some too long,
+\t\tSome sell and others buy;
+\n\t\tSome do the deed with many tears,
+\t\t\t\tAnd some without sigh:
+\n\t\t\t\tFor each man kills the thing he loves,
+\t\t\t\t\t\tYet each man does not die."""
+      
+              
+                 
+print(metin)
+            
+                     
